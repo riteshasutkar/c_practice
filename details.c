88bd6ac7d7994bd6f2126358bb27a570f846bbf2
riteshasutkar <asutkarritesh@gmail.com>
2019-06-23 15:19:00 +0530

    initial commit

---
+++ details.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("name-ritesh\n");
+    printf("age-19\n");
+    printf("city-wani\n");
+    return 0;
+}       
---
+++ face.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf(" _   _ \n");
+    printf("   o\n");
+    printf("  ---\n");
+    return 0;
+}
+
+
