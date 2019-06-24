32ca8f9cf37cf726b8b18912f4eedd3cdc8463d2
riteshasutkar <asutkarritesh@gmail.com>
2019-06-24 11:45:41 +0530

    add month.c

--- month.c
+++ month.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include<stdio.h> 
 int main()
-{
-    int months, days;
+    {
+    int months=0, days=0;
     printf("Enter days\n");
     scanf("%d", &days);
-months=days/30;
-days=days%30;
-printf("months=%d Days=%d",months,days);
-return 4;
-}
+    months=days/30;
+    days=days%30;
+    printf("months=%d Days=%d",months,days);
+    return 0;
+    }
