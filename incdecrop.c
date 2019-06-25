e5c7f767f5b3806090b58c3fd3f842e9d4faa8f2
riteshasutkar <asutkarritesh@gmail.com>
2019-06-25 12:17:24 +0530

    send

---
+++ incdecrop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    a=3;
+    b=51;
+    printf("hmmm..\n");
+    printf("%d\n",a+b);
+    printf("%d\n",a+++b);
+    printf("%d\n",++a+ ++b);
+    return 0;
+
+}
+
