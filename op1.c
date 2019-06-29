f5f4443332d87780d7fc19bece97c863b11c2efd
riteshasutkar <asutkarritesh@gmail.com>
2019-06-29 09:33:48 +0530

    i send

---
+++ op1.c
@@ -0,0 +1,18 @@
+                                                             
+ #include<stdio.h>
+ int main()
+ {    int a,b,c,d,e;
+      printf("a=");
+      scanf("%d",&a);
+      printf("b=");
+      scanf("%d",&b);
+      c=a/b;
+      d=a/b*c;
+      e=a-b;
+      printf("%d\n",c);
+      printf("%d\n",d);
+      printf("%d\n",e);
+      return 0;
+ }
+ 
+
